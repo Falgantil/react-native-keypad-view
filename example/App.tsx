c07e70c188635027c4ee0c885f4534d1fa50bc6a
Bjarke <Bjarke@FALGANTIL-PC>
2023-06-26 15:44:16 +0200

    Exclude node_modules

--- example/App.tsx
+++ example/App.tsx
@@ -1,12 +1,41 @@
+import Color from 'color';
 import KeyPadScreen from 'react-native-keypad-view';
 
 export default function App() {
   return (
     <KeyPadScreen
-      text='Enter PIN'
+      length={12}
+      text='Hand over ye treasured PIN, rapscallion!'
       onSubmit={async () => {
         await new Promise<void>(r => setTimeout(r, 500));
         return false;
+      }}
+      style={{
+        containerStyle: {
+          backgroundColor: '#2B2D5B'
+        },
+        textStyle: {
+          color: 'white',
+          fontStyle: 'italic',
+          fontSize: 18
+        },
+        keyPadInputStyle: {
+          borderColor: 'white',
+          borderWidth: 1,
+          size: 20,
+          spacing: 3,
+          filledBackground: Color('#2B2D5B').lighten(2).hex()
+        },
+        keyPadButtonStyle: {
+          backgroundColor: '#2B2D5B',
+          borderColor: Color('#2B2D5B').lighten(1).hex(),
+          borderWidth: 4,
+          textStyle: {
+            fontWeight: '900',
+            color: 'white',
+            transform: [{ rotateZ: '-20deg' }]
+          }
+        }
       }}
     />
   );
